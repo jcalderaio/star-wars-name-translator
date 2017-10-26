7d42793f22eab1407eaad213a933387944cf064f
John Calderaio <jcalderaio@gmail.com>
2017-10-26 17:05:43 -0400

    first push

---
+++ StarWars.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class StarWars {
+    public static void main(String args[]) {
+        StarWarsNameGenerator sw = new StarWarsNameGenerator();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is your first name?");
+        String fn = scan.next();
+        System.out.println("What is your last name?");
+        String ln = scan.next();
+        System.out.println("What is your mother's maiden name?");
+        String mn = scan.next();
+        System.out.println("What city were you born in?");
+        String cb = scan.next();
+        sw.fullName(fn, ln, mn, cb);
+    }
+}
+
+class StarWarsNameGenerator
+{
+    private String firstName;
+    private String lastName;
+
+    public void fullName(String firstName, String lastName, String maiden, String cityBornIn) {
+        this.firstName = lastName.substring(0,3).concat(firstName.substring(0,2));
+        this.lastName = maiden.substring(0,2).concat(cityBornIn.substring(0,3));
+        System.out.println(this.firstName + " " + this.lastName);
+    }
+}
